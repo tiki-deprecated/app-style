1ca06a1769a142fd02e8a7c98534a8f486e8d9d7
Ricardo Gonçalves <ricardo@mytiki.com>
2022-04-08 13:00:24 -0300

    init

---
+++ lib/style.dart
@@ -0,0 +1,7 @@
+library style;
+
+export 'size_provider.dart';
+export 'color_provider.dart';
+export 'font_provider.dart';
+export 'icon_provider.dart';
+export 'image_provider.dart';
---
+++ test/style_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:style/style.dart';
+
+void main() {}
