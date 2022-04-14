2291d517a0819a5072592184a3eb25eed59b420a
Ricardo Gonçalves <ricardo@mytiki.com>
2022-04-14 11:05:08 -0300

    text provider

--- lib/style.dart
+++ lib/style.dart
@@ -2,6 +2,8 @@ library style;
 
 export 'size_provider.dart';
 export 'color_provider.dart';
-export 'font_provider.dart';
+export 'text_provider.dart';
 export 'icon_provider.dart';
 export 'image_provider.dart';
+
+class Style{}
