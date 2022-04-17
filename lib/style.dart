0ca27da5e636e7097283967bb61cc9b98d64e274
Ricardo Gonçalves <ricardo@mytiki.com>
2022-04-17 02:43:18 -0300

    login images ok

--- lib/style.dart
+++ lib/style.dart
@@ -2,12 +2,14 @@ library style;
 
 import 'package:flutter/material.dart';
 import 'src/size_provider.dart' as sizer;
+import 'src/image_provider.dart' as img;
 
 export 'src/size_provider.dart';
 export 'src/color_provider.dart';
 export 'src/text_provider.dart';
 export 'src/icon_provider.dart';
 export 'src/image_provider.dart';
+export 'src/animation_provider.dart';
 
 class Style{
 
