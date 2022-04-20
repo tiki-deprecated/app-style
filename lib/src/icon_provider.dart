b18c42744037c8e8c4301ea7897207415975a1c2
Ricardo Gonçalves <ricardo@mytiki.com>
2022-04-20 09:45:16 -0300

    login images ok

--- lib/src/icon_provider.dart
+++ lib/src/icon_provider.dart
@@ -6,12 +6,20 @@ class IconProvider{
   IconProvider._();
 
   static const _kFontFam = 'TikiIcons';
-  static const _kFontLogin = 'LoginIcons';
   static const String? _kFontPkg = 'style';
 
   static const IconData account_box = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData close = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_back = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_downward = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_forward = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_upward = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData keyboard_arrow_down = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData keyboard_arrow_left = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData keyboard_arrow_right = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData keyboard_arrow_up = IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ccw = IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bug_report = IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData account_balance_wallet = IconData(0xf008, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData arrow = IconData(0xe800, fontFamily: _kFontLogin, fontPackage: _kFontPkg);
-  static const IconData x = IconData(0xe801, fontFamily: _kFontLogin, fontPackage: _kFontPkg);
-  static const IconData reload = IconData(0xe808, fontFamily: _kFontLogin, fontPackage: _kFontPkg);
+  static const IconData light_bulb = IconData(0xf30a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
